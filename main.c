a4a5f2ca6a74446f855186b327502eb97e7c8edd
Nicole Gaia <nicgaia01@gmail.com>
2026-07-22 20:51:31 -0300

    Salvando alterações

--- main.c
+++ main.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include "cachoeira.h"
+
+int main() {
+    Municipio *municipio = inicializar_lista();
+    municipio = inserir_municipio(municipio, 1, "Uberlândia");
+    municipio = inserir_municipio(municipio, 2, "Uberaba");
+
+    listar_municipios(municipio);
+
+    remover_municipio(municipio, 2);
+    listar_municipios(municipio);
+
+
+
+    return 0;
+}
