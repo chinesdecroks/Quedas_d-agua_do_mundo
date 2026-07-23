baa304453cbaf2e94b49e6c6e2e163413bab58bd
Nicole Gaia <nicgaia01@gmail.com>
2026-07-22 21:04:34 -0300

    Incluindo funções com ponteiro duplo feitas pelo Leo

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "cachoeira.h"
 
-int main() {
-    Municipio *municipio = inicializar_lista();
-    municipio = inserir_municipio(municipio, 1, "Uberlândia");
-    municipio = inserir_municipio(municipio, 2, "Uberaba");
+int main(){
+    Municipio* lmun = inicializar_lista_vazia();
+    inserir_municipio(&lmun, 1, "Uberlândia");
 
-    listar_municipios(municipio);
+    inserir_cachoeira(&lmun, 1, 1, "Água brilhante", 9, "Fácil");
+    inserir_cachoeira(&lmun, 1, 2, "Pedra solta", 14, "Díficil");
+    inserir_cachoeira(&lmun, 1, 3, "Pedra lascada", 20, "Díficil");
+    listar_cachoeiras_municipio(lmun, 1);
 
-    remover_municipio(municipio, 2);
-    listar_municipios(municipio);
-
-
+    alterar_cachoeira(lmun, 1, 2, "Cristalina", 20, "Difícil");
+    listar_cachoeiras_municipio(lmun, 1);
 
     return 0;
 }
